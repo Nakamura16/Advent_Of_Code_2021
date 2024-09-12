5e6a7fedc9d52051b10d7072cb8a1ef46a96ec2d
Nakamura16 <andre.massao.nakamura@gmail.com>
2024-09-12 15:27:25 -0300

    Added the new FileReader Project, Implemented method for reading and tested it

---
+++ AdventOfCode2021/FileReader/FileReader.cs
@@ -0,0 +1,14 @@
+﻿namespace FileReader;
+
+public class FileReader
+{
+    public IReadOnlyList<string> ReadFile(string filePath)
+    {
+        var lines = File.ReadLines(filePath).ToList();
+        if (!lines.Any())
+        {
+            throw new Exception("The current file is empty.");
+        }
+        return lines;
+    }
+}
