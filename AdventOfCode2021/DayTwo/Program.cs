a7707811101541892241403a521dc6acc1917ddd
Nakamura16 <andre.massao.nakamura@gmail.com>
2024-09-12 17:58:26 -0300

    Solution for Day Two part One

---
+++ AdventOfCode2021/DayTwo/Program.cs
@@ -0,0 +1,40 @@
+﻿using FileReader.Application;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var reader = new FileReaderTool();
+        var filePath = "C:\\Users\\andre\\Desktop\\estudos C#\\"
+            + "Advent_Of_Code_2021\\AdventOfCode2021\\DayTwo\\input.txt";
+
+        var instructions = reader.ReadFile(filePath).Select(instruction =>
+        {
+            var instructionWords = instruction.Split(" ");
+            var direction = instructionWords[0];
+            var steps = int.Parse(instructionWords[1]);
+            return (direction, steps);
+        });
+
+        var horizontalPosition = 0;
+        var depthPosition = 0;
+
+        foreach (var instruction in instructions)
+        {
+            switch (instruction.direction)
+            {
+                case "forward":
+                    horizontalPosition += instruction.steps; 
+                    break;
+                case "down":
+                    depthPosition += instruction.steps;
+                    break;
+                case "up":
+                    depthPosition -= instruction.steps;
+                    break;
+            }
+        }
+
+        Console.WriteLine($"Solution for Day Two: {horizontalPosition * depthPosition}");
+    }
+}
