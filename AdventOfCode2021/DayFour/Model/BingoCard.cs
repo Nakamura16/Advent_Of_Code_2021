a35b19d28d3d3f122811576c9bec698d36800d70
Nakamura16 <andre.massao.nakamura@gmail.com>
2024-09-26 13:19:50 -0300

    Moved Number to another class

--- AdventOfCode2021/DayFour/Model/BingoCard.cs
+++ AdventOfCode2021/DayFour/Model/BingoCard.cs
@@ -1,15 +1,3 @@
 ﻿namespace DayFour.Model;
 
 public record BingoCard(List<List<Number>> Numbers);
-
-public class Number
-{
-    public int Value { get; set; }
-    public bool IsChecked { get; set; }
-
-    public Number(int value, bool isChecked = false)
-    {
-        Value = value;
-        IsChecked = isChecked;
-    }
-};
