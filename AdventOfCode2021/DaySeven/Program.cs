fe4d64a88a8cb44d930108ee344f3d0782d44107
Andre Nakamura <andremnnakamura@gmail.com>
2024-10-11 09:36:44 -0300

    Setup workbench

---
+++ AdventOfCode2021/DaySeven/Program.cs
@@ -0,0 +1,12 @@
+﻿using FileReader.Application;
+
+namespace DaySeven;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var filePath = "C:\\Andre\\Estudos\\Advent_Of_Code_2021\\AdventOfCode2021\\DaySeven\\Input.txt";
+        var file = new FileReaderTool().ReadFile(filePath).ToList();
+    }
+}
