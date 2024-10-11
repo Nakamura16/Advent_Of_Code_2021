f7b8ee6c64ae466f1ec8ba39d2131bce10bc823d
Andre Nakamura <andremnnakamura@gmail.com>
2024-10-11 10:00:04 -0300

    Solution for partOne Found

--- AdventOfCode2021/DaySeven/Program.cs
+++ AdventOfCode2021/DaySeven/Program.cs
@@ -8,5 +8,24 @@ public class Program
     {
         var filePath = "C:\\Andre\\Estudos\\Advent_Of_Code_2021\\AdventOfCode2021\\DaySeven\\Input.txt";
         var file = new FileReaderTool().ReadFile(filePath).ToList();
+
+        var positions = file[0].Split(",").Select(position => int.Parse(position)).ToList();
+
+        var median = GetMedian(positions);
+        var solution = 0;
+        foreach (var position in positions)
+        {
+            var usedFuel = Math.Abs(position - median);
+            solution += usedFuel;
+        }
+
+        Console.WriteLine($"PartOne Solution: {solution}");
+    }
+
+    private static int GetMedian(List<int> positions)
+    {
+        var copy = new List<int>(positions);
+        copy.Sort();
+        return copy[copy.Count / 2];
     }
 }
